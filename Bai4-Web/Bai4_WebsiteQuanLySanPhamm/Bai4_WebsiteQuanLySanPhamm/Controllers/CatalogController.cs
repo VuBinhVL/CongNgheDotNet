8aa36eeb761b34daeca27d3e86a349635868c207
Vu Binh <22520129@gm.uit.edu.vn>
2024-10-04 16:00:26 +0700

    Hoàn thành bài 4

---
+++ Bai4-Web/Bai4_WebsiteQuanLySanPhamm/Bai4_WebsiteQuanLySanPhamm/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai4_WebsiteQuanLySanPhamm;
+using Bai4_WebsiteQuanLySanPhamm.Models;
+
+namespace Bai4_WebsiteQuanLySanPhamm.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            string connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=QuanLySanPham;Integrated Security=True";
+			QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionString);
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ Bai4-Web/Bai4_WebsiteQuanLySanPhamm/Bai4_WebsiteQuanLySanPhamm/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai4_WebsiteQuanLySanPhamm;
+using Bai4_WebsiteQuanLySanPhamm.Models;
+
+namespace Bai4_WebsiteQuanLySanPhamm.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+			string connectionString = @"Data Source=LAPTOP-J7OECDJF\SQLEXPRESS;Initial Catalog=QuanLySanPham;Integrated Security=True";
+            QuanLySanPhamDataContext dataContext = new QuanLySanPhamDataContext(connectionString);
+            List<Product> dsProducts = dataContext.Products.ToList();
+			return View(dsProducts);
+        }
+    }
+}
